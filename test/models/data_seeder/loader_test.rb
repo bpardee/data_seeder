f4234282b7aa47bbe9ac63fe3f2ba0fe48d66f0f
Brad Pardee <bradpardee@gmail.com>
2017-01-11 07:49:36 -0500

    Better cleanup of test

--- test/models/data_seeder/loader_test.rb
+++ test/models/data_seeder/loader_test.rb
@@ -7,17 +7,18 @@ describe DataSeeder::Loader, :model do
     before do
       @name      = 'test_loader_logger'
       @seed_dirs = setup_seed_dirs(@name, 'states_txt')
+      @logfile   = Rails.root.join('tmp', "loader_test.log.#{$$}")
     end
 
     after do
       cleanup_seed_dir(@name)
+      FileUtils.rm(@logfile)
     end
 
     it 'should load seed files' do
-      logfile = Rails.root.join('tmp', "loader_test.log.#{$$}")
-      loader = DataSeeder::Loader::Txt.new(logger: Logger.new(logfile))
+      loader = DataSeeder::Loader::Txt.new(logger: Logger.new(@logfile))
       loader.logger.info('hello foobar')
-      buf = File.read(logfile)
+      buf = File.read(@logfile)
       assert buf.match(/hello foobar/)
     end
   end
